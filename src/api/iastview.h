1e47dc185a834d7ccc7d8ca4d78aea1360281416
visualfc <visualfc@gmail.com>
2011-01-20 16:59:51 +0800

    iastview.h

---
+++ src/api/iastview.h
@@ -0,0 +1,23 @@
+#ifndef __LITEAPI_IASTVIEW_H__
+#define __LITEAPI_IASTVIEW_H__
+
+#include <QObject>
+#include <QString>
+#include <QByteArray>
+
+class IAstView
+{
+public:
+    virtual ~IAstView() {}
+    virtual void update(const QString &fileName, const QByteArray &data) = 0;
+};
+
+class IAstViewFactory
+{
+public:
+    virtual ~IAstViewFactory() {}
+    virtual QStringList fileTypes() = 0;
+    virtual IAstView *load(const QString &fileName) = 0;
+};
+
+#endif // __LITEAPI_IASTVIEW_H__
